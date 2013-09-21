6c10d65419a89ca946c6fbba861bca0441a36fe7
sebastian <salexis147@gmail.com>
2013-09-21 13:44:42 -0300

    respaldo

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,10 @@
+class WelcomeController < ApplicationController
+  
+  def index
+  	if  user_signed_in?
+  		redirect_to :controller=>'dashboard', :action => 'index'
+  	end
+
+  end
+  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Devise::Application.config.secret_key_base = 'a3f742e20c2c6893913dde36aae81123356377df7b9661b5b9fc83c2e80726830e75d6a1f5b7c77edc388b663637b3d367291fe4ce5f906d2c76c8bbabe042be'
